2500f78ee5fc494bcdd405ccf29ddd01645a6b98
Anil Kumar Dasari <anild@Anils-MacBook-Air.local>
2022-12-18 17:52:03 -0500

    Task Manager working code

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TaskNavigator from './Navigation/TaskNavigator';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import FlashMessage from "react-native-flash-message";
 
+const store = configureStore()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store = { store }>
+      <TaskNavigator />
+      <FlashMessage
+          accessibilityLabel={'toast'}
+          accessible={true}
+          accessibilityRole={'text'}
+          position="bottom"
+          duration={3000}
+        />
+    </Provider>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
